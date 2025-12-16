c6eba61905a1dab245848651b75e322bd3bf08fe
linkbilalali <link.bilalali@gmail.com>
2025-12-16 12:44:00 +0000

    Tuning LSTM

---
+++ *.py
@@ -0,0 +1,120 @@
+# Optimization step 5 - 2025-12-28
+# Optimization step 8 - 2026-01-02
+# Optimization step 12 - 2025-12-25
+# Optimization step 14 - 2026-01-07
+# Optimization step 16 - 2026-01-10
+# Optimization step 21 - 2025-12-25
+# Optimization step 25 - 2025-12-16
+# Optimization step 26 - 2025-12-23
+# Optimization step 33 - 2026-01-09
+# Optimization step 34 - 2025-12-29
+# Optimization step 35 - 2025-12-25
+# Optimization step 36 - 2026-01-08
+# Optimization step 37 - 2025-12-17
+# Optimization step 39 - 2025-12-25
+# Optimization step 44 - 2026-01-10
+# Optimization step 45 - 2026-01-01
+# Optimization step 49 - 2025-12-29
+# Optimization step 56 - 2025-12-26
+# Optimization step 62 - 2025-12-23
+# Optimization step 66 - 2025-12-26
+# Optimization step 71 - 2026-01-07
+# Optimization step 74 - 2025-12-27
+# Optimization step 82 - 2025-12-19
+# Optimization step 87 - 2025-12-31
+# Optimization step 89 - 2026-01-05
+# Optimization step 93 - 2025-12-28
+# Optimization step 96 - 2025-12-26
+# Optimization step 99 - 2025-12-23
+# Optimization step 104 - 2026-01-02
+# Optimization step 106 - 2026-01-05
+# Optimization step 107 - 2025-12-31
+# Optimization step 108 - 2026-01-03
+# Optimization step 109 - 2025-12-17
+# Optimization step 110 - 2026-01-06
+# Optimization step 111 - 2026-01-08
+# Optimization step 113 - 2026-01-04
+# Optimization step 115 - 2026-01-01
+# Optimization step 116 - 2025-12-30
+# Optimization step 117 - 2025-12-22
+# Optimization step 118 - 2026-01-03
+# Optimization step 120 - 2025-12-22
+# Optimization step 135 - 2025-12-25
+# Optimization step 136 - 2025-12-28
+# Optimization step 140 - 2025-12-26
+# Optimization step 141 - 2026-01-08
+# Optimization step 143 - 2026-01-08
+# Optimization step 144 - 2026-01-02
+# Daily Update: 2025-12-17 - Log ID 2257
+# Daily Update: 2025-12-18 - Log ID 28306
+# Daily Update: 2025-12-20 - Log ID 28908
+# Daily Update: 2025-12-20 - Log ID 12949
+# Daily Update: 2025-12-21 - Log ID 32561
+# Daily Update: 2025-12-21 - Log ID 8200
+# Daily Update: 2025-12-22 - Log ID 1658
+# Daily Update: 2025-12-23 - Log ID 32261
+# Daily Update: 2025-12-24 - Log ID 8626
+# Daily Update: 2025-12-27 - Log ID 18701
+# Daily Update: 2025-12-28 - Log ID 1565
+# Daily Update: 2025-12-29 - Log ID 24009
+# Daily Update: 2025-12-30 - Log ID 16361
+# Daily Update: 2025-12-31 - Log ID 18526
+# Daily Update: 2026-01-01 - Log ID 21675
+# Daily Update: 2026-01-02 - Log ID 26442
+# Daily Update: 2026-01-02 - Log ID 6678
+# Daily Update: 2026-01-04 - Log ID 4197
+# Daily Update: 2026-01-05 - Log ID 16708
+# Daily Update: 2026-01-05 - Log ID 10546
+# Daily Update: 2026-01-06 - Log ID 16629
+# Daily Update: 2026-01-06 - Log ID 4012
+# Daily Update: 2026-01-08 - Log ID 22073
+# Daily Update: 2026-01-08 - Log ID 28246
+# Daily Update: 2026-01-08 - Log ID 648
+# Daily Update: 2026-01-09 - Log ID 14118
+# Daily Update: 2026-01-09 - Log ID 27485
+# Daily Update: 2026-01-09 - Log ID 9663
+# Daily Update: 2026-01-10 - Log ID 18928
+# Daily Update: 2026-01-10 - Log ID 26949
+# Daily Update: 2025-12-16 - Log ID 10142
+# Daily Update: 2025-12-16 - Log ID 17501
+# Daily Update: 2025-12-17 - Log ID 5356
+# Daily Update: 2025-12-18 - Log ID 28110
+# Daily Update: 2025-12-19 - Log ID 11989
+# Daily Update: 2025-12-20 - Log ID 16091
+# Daily Update: 2025-12-21 - Log ID 629
+# Daily Update: 2025-12-22 - Log ID 29371
+# Daily Update: 2025-12-22 - Log ID 22480
+# Daily Update: 2025-12-23 - Log ID 7626
+# Daily Update: 2025-12-24 - Log ID 22035
+# Daily Update: 2025-12-24 - Log ID 17338
+# Daily Update: 2025-12-24 - Log ID 18692
+# Daily Update: 2025-12-25 - Log ID 27129
+# Daily Update: 2025-12-25 - Log ID 32207
+# Daily Update: 2025-12-26 - Log ID 12437
+# Daily Update: 2025-12-27 - Log ID 18317
+# Daily Update: 2025-12-27 - Log ID 22770
+# Daily Update: 2025-12-27 - Log ID 6995
+# Daily Update: 2025-12-28 - Log ID 7594
+# Daily Update: 2025-12-28 - Log ID 12473
+# Daily Update: 2025-12-28 - Log ID 2233
+# Daily Update: 2025-12-28 - Log ID 25098
+# Daily Update: 2025-12-28 - Log ID 25946
+# Daily Update: 2025-12-30 - Log ID 29746
+# Daily Update: 2025-12-30 - Log ID 25835
+# Daily Update: 2025-12-30 - Log ID 17693
+# Daily Update: 2025-12-30 - Log ID 12114
+# Daily Update: 2025-12-30 - Log ID 3707
+# Daily Update: 2026-01-01 - Log ID 15237
+# Daily Update: 2026-01-01 - Log ID 18974
+# Daily Update: 2026-01-03 - Log ID 28381
+# Daily Update: 2026-01-04 - Log ID 12425
+# Daily Update: 2026-01-05 - Log ID 3996
+# Daily Update: 2026-01-05 - Log ID 27009
+# Daily Update: 2026-01-06 - Log ID 6294
+# Daily Update: 2026-01-06 - Log ID 23715
+# Daily Update: 2026-01-06 - Log ID 8708
+# Daily Update: 2026-01-07 - Log ID 15122
+# Daily Update: 2026-01-08 - Log ID 25467
+# Daily Update: 2026-01-09 - Log ID 11753
+# Daily Update: 2026-01-10 - Log ID 8695
+# Dev Log: 2025-12-16 revision 1
